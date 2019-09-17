43e22eb085629077ca76122351d6e356a9467ee8
Birhanu Bishaw <200414083@student.georgianc.on.ca>
2019-09-16 20:09:36 -0400

    Add project files.

---
+++ COMP2084-MusicStore/Controllers/StoreController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP2084_MusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Ok("Hello World from Store.Index()");
+        }
+        public IActionResult Browse(string genre)
+        {
+            genre = HttpUtility.HtmlEncode("Store.Browse, genre = " + genre);
+            return Ok(genre);
+        }
+        public IActionResult Details(int id)
+        {
+            return Ok(id.ToString());
+        }
+    }
+
+}
