53473f421020ced849618fa908e5ea83d7f1ecfa
Birhanu Bishaw <200414083@student.georgianc.on.ca>
2019-09-30 16:54:31 -0400

    Add some files Album Genre Artist Readme

--- COMP2084-MusicStore/Models/Album.cs
+++ COMP2084-MusicStore/Models/Album.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +9,18 @@ namespace COMP2084_MusicStore.Models
 {
     public class Album
     {
-        public string Name { get; set; }
+        [Key]
+        public int AlbumId { get; set; }
+
+        [ForeignKey("Genre")]
+        public int GenreId { get; set; }
+
+        public string Title { get; set; }
+
         public string Artist { get; set; }
+
         public DateTime ReleaseDate { get; set; }
+
+        public ICollection<Song> Songs { get; set; }
     }
 }
---
+++ COMP2084-MusicStore/Models/Artist.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COMP2084_MusicStore.Models
+{
+    public class Artist
+    {
+    }
+}
